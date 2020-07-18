f8237790085573d61bcf28ef002d53818eaf457d
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-07-18 12:20:50 +0800

    add test

---
+++ BasicSyntax/operator/opt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+a = 21
+b = 10
+c = 30
+d = 2
+
+if a == b:
+    print 'a = b'
+else:
+    print 'a != b'
+
+if a >= c:
+    print 'a >= c'
+else:
+    print 'a !>= c'
+
+print d ** 5
