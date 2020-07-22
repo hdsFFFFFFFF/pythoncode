ae2105d15a6cbecee2f2d2eb7fb4bdb8683d74d5
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-07-22 17:42:04 +0800

    add new note

---
+++ object/parent_child.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class Parent:   #定义父类
+    parentAttr = 100
+
+    def __init__(self):
+        print '调用父类构造函数'
+
+    def parentMethod(self):
+        print '调用父类方法'
+
+    def setAttr(self, attr):
+        Parent.parentAttr = attr
+
+    def getAttr(self):
+        print '父类属性：', Parent.parentAttr
+
+class Child(Parent):    #定义子类
+    def __init__(self):
+        print '调用子类的构造方法'
+
+    def childMethod(self):
+        print '调用子类的方法'
+
+c = Child()
+c.childMethod()
+c.parentMethod()
+c.setAttr(200)
+c.getAttr()
+        
--- object/test_BPF.py
+++ object/test_BPF.py
@@ -14,6 +14,9 @@ b = BPF(text = '''
 print b
 print b.__doc__     #查看类的帮助信息
 print b.__class__   #查看对象所属的类
+print BPF.__name__
+print b.__module__
+print b.__dict__
 
 #运行结果
 #   <bcc.BPF object at 0x7f18b8e6b350>
